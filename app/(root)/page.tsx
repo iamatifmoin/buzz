2b4060ba0c543a4cef7aa8c3014a3b0158637a4d
iamatifmoin <atifmoin3@gmail.com>
2024-01-18 18:50:15 +0530

    bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="head-text text left">Home</h1>
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
