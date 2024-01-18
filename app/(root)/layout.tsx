300788dba232a23062e4f88521c7bbe09edbb5e8
DeshDeepakKushwaha <deshdeepakkushwah04@gmail.com>
2024-01-18 16:19:14 +0530

    rightsidebar added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
-            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
