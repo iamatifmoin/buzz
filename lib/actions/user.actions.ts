51022b1ac62a9682f2ac7b66c63717c95e376bda
iamatifmoin <atifmoin3@gmail.com>
2024-01-21 22:47:03 +0530

    added profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,7 +6,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
-
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -48,16 +48,35 @@ export async function updateUser({
   }
 }
 
-
-export async function fetchUser(
-  userId:string) {
+export async function fetchUser(userId: string) {
   try {
     connectToDB();
 
-
-    return await User.findOne({id:userId})
+    return await User.findOne({ id: userId });
   } catch (error) {
     console.error("Error fetching users:", error);
     throw error;
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
