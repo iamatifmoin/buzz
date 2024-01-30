d44766b5c56d5f1c7a8579d37fd911caf6a9ca2a
iamatifmoin <atifmoin3@gmail.com>
2024-01-30 15:56:20 +0530

    centered auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}><div className="w-full flex justify-center items-center minh-screen">{children}</div></body>
+        <body className={`${inter.className} bg-dark-1`}><div className="w-full flex justify-center items-center min-h-screen">{children}</div></body>
       </html>
     </ClerkProvider>
   );
