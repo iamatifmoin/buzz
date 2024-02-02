78a078dca936f042db07934fd93d784181610f17
iamatifmoin <atifmoin3@gmail.com>
2024-02-02 14:43:12 +0530

    fixed time error in community post

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -16,7 +16,15 @@ const threadSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: () => {
+      var currentTime = new Date();
+      var currentOffset = currentTime.getTimezoneOffset();
+      var ISTOffset = 330; // IST offset UTC +5:30
+      var ISTTime = new Date(
+        currentTime.getTime() + (ISTOffset + currentOffset) * 60000
+      );
+      return ISTTime;
+    },
   },
   parentId: {
     type: String,
